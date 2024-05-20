8d42f208388ac9931dbc52459658e853296a80f6
kkdd <kkondo_z0@nifty.com>
2024-05-20 17:00:28 +0900

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 
 //import {KEY_MAPTILER} from './key/maptiler.js'
-import keyJson from './key.js';
+import keyJson from './key.json'; assert { type: 'json' }
 const KEY_MAPTILER = keyJson["maptilerKey"];
 // const KEY_MAPTILER = 'YOUR_MAPTILER_API_KEY_HERE';
 
